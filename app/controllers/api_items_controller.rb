ddc639afbcd5123d7b3d388af1e26710caca0022
Melissa <jstmac@gmail.com>
2019-06-14 10:43:37 -0600

    initial commit

---
+++ app/controllers/api_items_controller.rb
@@ -0,0 +1,2 @@
+class ApiItemsController < ApplicationController
+end
---
+++ test/controllers/api_items_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ApiItemsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
