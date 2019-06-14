2b8e6749e175cda24443f17f315e426adbb92680
Melissa <jstmac@gmail.com>
2019-06-14 11:30:25 -0600

    NavBar file created

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 const App = () => (
   <>
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NavBar = () => (
+  <>
+    <h1>Nav situation</h1>
+  </>
+);
+
+export default NavBar;
